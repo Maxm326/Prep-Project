5e26e7af2f595010aadf7c97bfca8801962d69cd
intern <maxm326@gmail.com>
2022-10-26 11:47:52 -0400

    Corrected EJBScheduler structure and tested

--- src/main/java/com/stackleader/EJBScheduler.java
+++ src/main/java/com/stackleader/EJBScheduler.java
@@ -1,7 +1,5 @@
 package com.stackleader;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 import javax.ejb.*;
 import javax.inject.Inject;
@@ -9,7 +7,7 @@ import java.sql.SQLException;
 import java.util.logging.Logger;
 
 @Startup
-@Stateless
+@Singleton
 public class EJBScheduler {
 
     @Inject
@@ -19,24 +17,8 @@ public class EJBScheduler {
     private TimerService timerService;
 
     private static final Logger LOGGER = Logger.getLogger(EJBScheduler.class.toString());
-
-    @PostConstruct
-    public void initialize() {
-        //FIXME: change back to 5 minutes (200000)
-        timerService.createTimer(0, 4000, "Every 5 minutes timer with no delay");
-    }
-
-    @Timeout
+    @Schedule(hour = "*", minute = "*/5", persistent = false)
     public void EJBTimeout(Timer timer) throws SQLException {
         LOGGER.info("************** Result Set Count = " + prepDao.getPrepData().size());
-    }
-
-    @PreDestroy
-    public void stop() {
-        System.out.println("EJB Timer: Stop timers.");
-        for (Timer timer : timerService.getTimers()) {
-            System.out.println("Stopping timer: " + timer.getInfo());
-            timer.cancel();
-        }
     }
 }
